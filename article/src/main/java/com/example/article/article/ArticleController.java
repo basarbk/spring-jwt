61c3fb5ce88c763112a53bb659491b3bb3bf0dd8
Basar Buyukkahraman <basarbk@yahoo.com>
2021-01-11 20:18:42 +0300

    Validate token

--- article/src/main/java/com/example/article/article/ArticleController.java
+++ article/src/main/java/com/example/article/article/ArticleController.java
@@ -8,7 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
 
 @RestController
 public class ArticleController {
@@ -22,7 +26,10 @@ public class ArticleController {
   }
 
   @PostMapping("/api/1.0/articles")
-  Article saveArticle(@RequestBody Article article){
+  Article saveArticle(@RequestBody Article article, @RequestHeader(name = "Authorization") String authorization){
+    String token = authorization.substring(7);
+    JwtParser parser = Jwts.parser().setSigningKey("my-app-secret");
+    parser.parse(token);
     return articleService.save(article);
   }
 
